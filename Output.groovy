6d88ad7e0707fe9111373851695d83a5b6b11b5c
Prasanth Deventhiran <88628112+PrasanthDeva18@users.noreply.github.com>
2025-08-25 17:28:34 +0530

    Update Output.groovy

--- Output.groovy
+++ Output.groovy
@@ -1,2 +1,9 @@
 println "Hello, Groovy!"
 println 'Hello world'
+
+println "5 + 4 = ${5+4}"
+
+//variable declaration and initialization
+def a = 10 // dynamic typing in grovy
+
+println "name : prasanth, roll_no : $a"
