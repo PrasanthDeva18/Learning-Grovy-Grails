408c586792c9b6f745305f93df45105f10817863
Prasanth Deventhiran <88628112+PrasanthDeva18@users.noreply.github.com>
2025-08-26 10:34:41 +0530

    Create sample-calc.groovy

---
+++ sample-calc.groovy
@@ -0,0 +1,10 @@
+def reader = System.in.newReader()
+
+int a = reader.readLine() as Integer;
+int b = reader.readLine() as Integer;
+
+println "A + B : ${a+b}"
+println "A - B : ${a-b}"
+println "A * B : ${a*b}"
+println "A / B : ${a/b}"
+println "A % B : ${a%b}"
