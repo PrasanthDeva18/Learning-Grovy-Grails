ed0678668699b9c1016a3ad341b2c6b23e72178b
Prasanth Deventhiran <88628112+PrasanthDeva18@users.noreply.github.com>
2025-08-26 10:11:17 +0530

    Create data-types.groovy

---
+++ data-types.groovy
@@ -0,0 +1,10 @@
+def a1 = 20 //dyanamically allocate the type
+
+int a = 10
+String b = "prasanth"
+boolean c = true
+double d = 19.99
+
+println "$a,$b,$c,$d"
+
+println "a = $a, b = $b, c = $c, d = $d"
