827773491f3f3440aa4b28cd4dcbaf051808e23d
Prasanth Deventhiran <88628112+PrasanthDeva18@users.noreply.github.com>
2025-08-26 10:26:54 +0530

    Update input.groovy

--- input.groovy
+++ input.groovy
@@ -3,4 +3,16 @@ def reader = System.in.newReader()
 println "Enter the integer"
 int a2 = reader.readLine() as Integer
 
+println "Enter the string"
+int b2 = reader.readLine()
+
+println "Enter the boolean"
+int c3 = reader.readLine().toBoolean();
+
+println "Enter the double"
+int d4 = reader.readLine() as Double
+
 println a2
+println b2
+println c3
+println d4
