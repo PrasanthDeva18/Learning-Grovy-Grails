131a27f477cae3bfad10cadfa9863a26eb8df9ec
Prasanth Deventhiran <88628112+PrasanthDeva18@users.noreply.github.com>
2025-08-26 10:49:51 +0530

    Update sample-calc.groovy

--- sample-calc.groovy
+++ sample-calc.groovy
@@ -3,8 +3,15 @@ def reader = System.in.newReader()
 int a = reader.readLine() as Integer;
 int b = reader.readLine() as Integer;
 
+boolean c = reader.readLine().toBoolean();
+
 println "A + B : ${a+b}"
 println "A - B : ${a-b}"
 println "A * B : ${a*b}"
 println "A / B : ${a/b}"
 println "A % B : ${a%b}"
+println "A ** B : ${a**b}"
+
+println a.getClass()
+println b.getClass()
+println c.getClass()
