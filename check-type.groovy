5bd8f47d58bacb41de5831dcee639c1704e21778
Prasanth Deventhiran <88628112+PrasanthDeva18@users.noreply.github.com>
2025-08-26 10:29:11 +0530

    Create check-type.groovy

---
+++ check-type.groovy
@@ -0,0 +1,11 @@
+int a = 10
+String b = "prasanth"
+boolean c = true
+double d = 19.99
+def e = [1, 2, 3]
+
+println "a type: " + a.getClass()
+println "b type: " + b.getClass()
+println "c type: " + c.getClass()
+println "d type: " + d.getClass()
+println "e type: " + e.getClass()
